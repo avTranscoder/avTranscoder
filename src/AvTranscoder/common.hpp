56ed507842ef3f75b0259760d0754f6d3eaf20b7
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2014-03-03 16:20:43 +0100

    clean build under Linux & clean indentation

--- src/AvTranscoder/common.hpp
+++ src/AvTranscoder/common.hpp
@@ -5,6 +5,10 @@ extern "C" {
 #ifndef __STDC_CONSTANT_MACROS
     #define __STDC_CONSTANT_MACROS
 #endif
+#ifndef INT64_C
+	#define INT64_C(c) (c ## LL)
+	#define UINT64_C(c) (c ## ULL)
+#endif
 #include <libavutil/version.h>
 #include <libavformat/version.h>
 #include <libavcodec/version.h>
