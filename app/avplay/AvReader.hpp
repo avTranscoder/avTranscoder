6dad33b7218eeac6d90c9188462074545f267b8e
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2014-05-25 04:30:18 -0400

    refactoring player code in C++

---
+++ app/avplay/AvReader.hpp
@@ -0,0 +1,119 @@
+#ifndef _AVPLAYER_AVREADER_
+#define _AVPLAYER_AVREADER_
+
+
+#include <AvTranscoder/InputStream.hpp>
+#include <AvTranscoder/InputStreamAudio.hpp>
+#include <AvTranscoder/InputStreamVideo.hpp>
+#include <AvTranscoder/InputFile.hpp>
+
+#include <AvTranscoder/ColorTransform.hpp>
+
+#include <AvTranscoder/Metadatas/Print.hpp>
+
+#include "Reader.hpp"
+
+class AvReader : public Reader
+{
+public:
+	AvReader( std::string filename )
+		: m_inputFile( filename )
+		, m_inputStreamVideo( NULL )
+		, m_sourceImage( NULL )
+		, m_imageToDisplay( NULL )
+	{
+		m_inputFile.analyse();
+		m_videoStream = m_inputFile.getProperties().videoStreams.at(0).streamId;
+
+		m_inputFile.readStream( m_videoStream );
+
+		m_inputStreamVideo = new avtranscoder::InputStreamVideo( m_inputFile.getStream( m_videoStream ) );
+		
+		m_sourceImage = new avtranscoder::Image( m_inputFile.getStream( m_videoStream ).getVideoDesc().getImageDesc() );
+
+		pixel.setBitsPerPixel( getComponents() * getBitDepth() );
+		pixel.setComponents( getComponents() );
+		pixel.setColorComponents( avtranscoder::eComponentRgb );
+		pixel.setSubsampling( avtranscoder::eSubsamplingNone );
+		pixel.setAlpha( false );
+		pixel.setPlanar( false );
+
+		imageDescToDisplay.setWidth ( m_sourceImage->desc().getWidth() );
+		imageDescToDisplay.setHeight( m_sourceImage->desc().getHeight() );
+		imageDescToDisplay.setDar   ( m_sourceImage->desc().getDar() );
+
+		imageDescToDisplay.setPixel ( pixel.findPixel() );
+		
+		m_imageToDisplay = new avtranscoder::Image( imageDescToDisplay );
+	}
+
+	~AvReader()
+	{
+		delete m_inputStreamVideo;
+		delete m_sourceImage;
+		delete m_imageToDisplay;
+	}
+
+	size_t getWidth()
+	{
+		return m_inputFile.getProperties().videoStreams.at(0).width;
+	};
+
+	size_t getHeight()
+	{
+		return m_inputFile.getProperties().videoStreams.at(0).height;
+	}
+
+	size_t getComponents()
+	{
+		return m_inputFile.getProperties().videoStreams.at(0).componentsCount;
+	}
+
+	size_t getBitDepth()
+	{
+		return 8;
+	}
+
+	const char* readNextFrame()
+	{
+		++currentFrame;
+		m_inputStreamVideo->readNextFrame( *m_sourceImage );
+		m_colorTransform.convert( *m_sourceImage, *m_imageToDisplay );
+		return (const char*)m_imageToDisplay->getPtr();
+	}
+
+	const char* readPrevFrame()
+	{
+		--currentFrame;
+		return readFrameAt( currentFrame );
+	}
+
+	const char* readFrameAt( const size_t frame )
+	{
+		// TODO seek @ frame
+
+		m_inputStreamVideo->readNextFrame( *m_sourceImage );
+		m_colorTransform.convert( *m_sourceImage, *m_imageToDisplay );
+		return (const char*)m_imageToDisplay->getPtr();
+	}
+
+	void printMetadatas()
+	{
+		displayMetadatas( m_inputFile );
+	}
+
+private:
+	avtranscoder::InputFile m_inputFile;
+	avtranscoder::InputStreamVideo* m_inputStreamVideo;
+
+	avtranscoder::Image* m_sourceImage;
+	avtranscoder::Image* m_imageToDisplay;
+
+	avtranscoder::Pixel pixel;
+	avtranscoder::ImageDesc imageDescToDisplay;
+
+	avtranscoder::ColorTransform m_colorTransform;
+	size_t m_videoStream;
+};
+
+#endif
