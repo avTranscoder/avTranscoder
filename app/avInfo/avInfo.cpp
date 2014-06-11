d2bbcf826d1206654148da6e99ad52cf7d758a0e
Clement Champetier <cnt@mikrosimage.eu>
2014-06-11 17:09:40 +0200

    AVInfo: display input / output extensions
    * This app checks getInputExtensions and getOutputExtensions functions.

--- app/avInfo/avInfo.cpp
+++ app/avInfo/avInfo.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 
 #include <AvTranscoder/Description.hpp>
 
@@ -8,5 +7,16 @@ int main( int argc, char** argv )
 	// std::cout << "avinfo" << std::endl;
 	std::vector<std::string> inputExtension  = avtranscoder::getInputExtensions();
 	std::vector<std::string> outputExtension = avtranscoder::getOutputExtensions();
+	
+	std::cout << "----- inputExtension -----" << std::endl;
+	std::cout << "nb inputExtension: " << inputExtension.size() << std::endl;
+	for( std::vector<std::string>::iterator it = inputExtension.begin(); it != inputExtension.end(); ++it )
+		std::cout << *it << std::endl;
+	
+	std::cout << "----- outputExtension -----" << std::endl;
+	std::cout << "nb outputExtension: " << outputExtension.size() << std::endl;
+	for( std::vector<std::string>::iterator it = outputExtension.begin(); it != outputExtension.end(); ++it )
+		std::cout << *it << std::endl;
+	
 	return 0;
 }
