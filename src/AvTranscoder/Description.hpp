0c05def6f79051653be0bc731cb30343cbf7a545
Clement Champetier <cnt@mikrosimage.eu>
2014-06-11 17:56:37 +0200

    Description: split into hpp and cpp
    Avoid potential multiple definitions when using the API.

--- src/AvTranscoder/Description.hpp
+++ src/AvTranscoder/Description.hpp
@@ -1,109 +1,17 @@
 #ifndef _AV_TRANSCODER_DESCRIPTION_HPP_
 #define _AV_TRANSCODER_DESCRIPTION_HPP_
 
-extern "C" {
-#ifndef __STDC_CONSTANT_MACROS
-    #define __STDC_CONSTANT_MACROS
-#endif
-#include <libavformat/avformat.h>
-#include <libavutil/avstring.h>
-}
-
 #include <vector>
-#include <cstring>
-#include <algorithm> //sort, unique
+#include <string>
 
 namespace avtranscoder
 {
 
-std::vector<size_t> getVersion()
-{
-	std::vector<size_t> version;
-	version.push_back( 0 );
-	version.push_back( 0 );
-	version.push_back( 0 );
-	// avutil_version
-	// avutil_license
-	return version;
-}
+std::vector<size_t> getVersion();
 
-std::vector<std::string> getInputExtensions()
-{
-	av_register_all();
-	std::vector<std::string> extensions;
-	AVInputFormat* iFormat = NULL;
+std::vector<std::string> getInputExtensions();
 
-	while( ( iFormat = av_iformat_next( iFormat ) ) )
-	{
-		if( iFormat->extensions != NULL )
-		{
-			// parse extensions
-			std::string exts = std::string( iFormat->extensions );
-			char* ext = strtok( const_cast<char*>( exts.c_str() ), "," );
-			while( ext != NULL )
-			{
-				extensions.push_back( std::string( ext ) );
-				ext = strtok( NULL, "," );
-			}
-			
-			// parse name (name's format defines (in general) extensions )
-			// don't need to do it in recent LibAV/FFMpeg versions
-			exts = std::string( iFormat->name );
-			ext = strtok( const_cast<char*>( exts.c_str() ), "," );
-			while( ext != NULL )
-			{
-				extensions.push_back( std::string( ext ) );
-				ext = strtok( NULL, "," );
-			}
-		}
-	}
-	// sort
-	std::sort( extensions.begin(), extensions.end() ); 
-	// suppress duplicates
-	std::vector<std::string>::iterator last = std::unique( extensions.begin(), extensions.end() );
-	extensions.erase( last, extensions.end() );
-	
-	return extensions;
-}
-
-std::vector<std::string> getOutputExtensions()
-{
-	av_register_all();
-	std::vector<std::string> extensions;
-	AVOutputFormat* oFormat = NULL;
-
-	while( ( oFormat = av_oformat_next( oFormat ) ) )
-	{
-		if( oFormat->extensions != NULL )
-		{			
-			// parse extensions
-			std::string exts = std::string( oFormat->extensions );
-			char* ext = strtok( const_cast<char*>( exts.c_str() ), "," );
-			while( ext != NULL )
-			{
-				extensions.push_back( std::string( ext ) );
-				ext = strtok( NULL, "," );
-			}
-			
-			// parse name (name's format defines (in general) extensions )
-			// don't need to do it in recent LibAV/FFMpeg versions
-			exts = std::string( oFormat->name );
-			ext = strtok( const_cast<char*>( exts.c_str() ), "," );
-			while( ext != NULL )
-			{
-				extensions.push_back( std::string( ext ) );
-				ext = strtok( NULL, "," );
-			}
-		}
-	}
-	// sort
-	std::sort( extensions.begin(), extensions.end() ); 
-	// suppress duplicates
-	std::vector<std::string>::iterator last = std::unique( extensions.begin(), extensions.end() );
-	extensions.erase( last, extensions.end() );
-	
-	return extensions;
-}
+std::vector<std::string> getOutputExtensions();
 
 }
 
