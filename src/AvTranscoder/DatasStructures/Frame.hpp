14ac3b979a50743b5f440165650d19334d47a0a8
Valentin Noel <van@mikrosimage.eu>
2014-06-16 17:59:46 +0200

    Add Frame base class for Image and AudioFrame into DataStructures

--- src/AvTranscoder/DatasStructures/Frame.hpp
+++ src/AvTranscoder/DatasStructures/Frame.hpp
@@ -1,17 +1,31 @@
 #ifndef _AV_TRANSCODER_DATA_FRAME_HPP_
 #define _AV_TRANSCODER_DATA_FRAME_HPP_
 
+#include <AvTranscoder/common.hpp>
+
 #include <string>
+#include <vector>
 
 namespace avtranscoder
 {
 
-class Frame
+typedef std::vector< unsigned char> DataBuffer;
+
+class AvExport Frame
 {
 public:
-	Frame(){};
 
-private:
+	Frame() {};
+
+	virtual DataBuffer& getBuffer() { return m_dataBuffer; }
+	virtual unsigned char* getPtr() { return &m_dataBuffer[0]; }
+#ifndef SWIG
+	virtual const unsigned char* getPtr() const { return &m_dataBuffer[0]; }
+#endif
+	virtual size_t getSize() const { return m_dataBuffer.size(); }
+	
+protected:
+	DataBuffer m_dataBuffer;
 
 };
 
