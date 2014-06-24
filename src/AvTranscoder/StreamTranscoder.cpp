d70c7a7fa3050cb7efc622d1a0299ff7d5aa16ad
Valentin Noel <van@mikrosimage.eu>
2014-06-24 10:59:21 +0200

    Remove polymorphism warning into StreamTranscoder

--- src/AvTranscoder/StreamTranscoder.cpp
+++ src/AvTranscoder/StreamTranscoder.cpp
@@ -8,7 +8,11 @@ StreamTranscoder::StreamTranscoder( AvInputStream& stream, OutputFile& outputFil
 	: _stream( &stream )
 	, _frameBuffer( NULL )
 	, _inputStreamReader( NULL )
+	, _inputStreamVideo( NULL )
+	, _inputStreamAudio( NULL )
 	, _outputStreamWriter( NULL )
+	, _outputStreamVideo( NULL )
+	, _outputStreamAudio( NULL )
 	, _outputFile( &outputFile )
 	, _streamIndex( streamId )
 	, _transcodeStream( false )
@@ -19,10 +23,16 @@ StreamTranscoder::~StreamTranscoder()
 {
 	if( _frameBuffer )
 		delete _frameBuffer;
-	if( _inputStreamReader )
-		delete _inputStreamReader;
-	if( _outputStreamWriter )
-		delete _outputStreamWriter;
+
+	if( _inputStreamVideo )
+		delete _inputStreamVideo;
+	if( _inputStreamAudio )
+		delete _inputStreamAudio;
+
+	if( _outputStreamVideo )
+		delete _outputStreamVideo;
+	if( _outputStreamAudio )
+		delete _outputStreamAudio;
 }
 
 void StreamTranscoder::init( const std::string& profile )
@@ -33,7 +43,8 @@ void StreamTranscoder::init( const std::string& profile )
 	{
 		case AVMEDIA_TYPE_VIDEO :
 		{
-			_inputStreamReader = new InputStreamVideo( *_stream );
+			_inputStreamVideo = new InputStreamVideo( *_stream );
+			_inputStreamReader = _inputStreamVideo;
 			_inputStreamReader->setup();
 
 			// re-wrap only, get output descriptor from input
@@ -43,18 +54,20 @@ void StreamTranscoder::init( const std::string& profile )
 				break;
 			}
 
-			OutputStreamVideo* outputStreamVideo = new OutputStreamVideo();
-			_outputStreamWriter = outputStreamVideo;
+			_outputStreamVideo = new OutputStreamVideo();
+			_outputStreamWriter = _outputStreamVideo;
 
 			_outputStreamWriter->setProfile( profile );
-			_outputFile->addVideoStream( outputStreamVideo->getVideoDesc() );
-			_frameBuffer = new Image( outputStreamVideo->getVideoDesc().getImageDesc() );
+			_outputFile->addVideoStream( _outputStreamVideo->getVideoDesc() );
+			_videoFrameBuffer = new Image( _outputStreamVideo->getVideoDesc().getImageDesc() );
+			_frameBuffer = _videoFrameBuffer;
 			
 			break;
 		}
 		case AVMEDIA_TYPE_AUDIO :
 		{
-			_inputStreamReader = new InputStreamAudio( *_stream );
+			_inputStreamAudio = new InputStreamAudio( *_stream );
+			_inputStreamReader = _inputStreamAudio;
 			_inputStreamReader->setup();
 
 			// re-wrap only, get output descriptor from input
@@ -64,12 +77,13 @@ void StreamTranscoder::init( const std::string& profile )
 				break;
 			}
 			
-			OutputStreamAudio* outputStreamAudio = new OutputStreamAudio();
-			_outputStreamWriter = outputStreamAudio;
+			_outputStreamAudio = new OutputStreamAudio();
+			_outputStreamWriter = _outputStreamAudio;
 
-			outputStreamAudio->setProfile( profile );
-			_outputFile->addAudioStream( outputStreamAudio->getAudioDesc() );
-			_frameBuffer = new AudioFrame( outputStreamAudio->getAudioDesc().getFrameDesc() );
+			_outputStreamWriter->setProfile( profile );
+			_outputFile->addAudioStream( _outputStreamAudio->getAudioDesc() );
+			_audioFrameBuffer = new AudioFrame( _outputStreamAudio->getAudioDesc().getFrameDesc() );
+			_frameBuffer = _audioFrameBuffer;
 			
 			break;
 		}
