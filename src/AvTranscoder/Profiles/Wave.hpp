11b61130316e587e46aebead2788dd86005281b9
Clement Champetier <cnt@mikrosimage.eu>
2014-07-03 13:56:04 +0200

    Profiles: update existing profiles
    * Use const name in Profile class.
    * Add avProfileFrameRate for video profile.

--- src/AvTranscoder/Profiles/Wave.hpp
+++ src/AvTranscoder/Profiles/Wave.hpp
@@ -12,10 +12,10 @@ void loadWave( Profile::ProfilesDesc& profiles )
 	wave24b48kMono[ Profile::avProfileIdentificatorHuman ] = "Wave 24bits 48kHz mono";
 	wave24b48kMono[ Profile::avProfileType ] = Profile::avProfileTypeAudio;
 
-	wave24b48kMono[ "codec" ] = "pcm_s24le";
-	wave24b48kMono[ "sample_fmt" ] = "s32";
-	wave24b48kMono[ "sample_rate" ] = "48000";
-	wave24b48kMono[ "channels" ] = "1";
+	wave24b48kMono[ Profile::avProfileCodec ] = "pcm_s24le";
+	wave24b48kMono[ Profile::avProfileSampleFormat ] = "s32";
+	wave24b48kMono[ Profile::avProfileSampleRate ] = "48000";
+	wave24b48kMono[ Profile::avProfileChannel ] = "1";
 
 	Profile::ProfileDesc wave16b48kMono;
 
@@ -23,10 +23,10 @@ void loadWave( Profile::ProfilesDesc& profiles )
 	wave16b48kMono[ Profile::avProfileIdentificatorHuman ] = "Wave 16bits 48kHz mono";
 	wave16b48kMono[ Profile::avProfileType ] = Profile::avProfileTypeAudio;
 
-	wave16b48kMono[ "codec" ] = "pcm_s16le";
-	wave16b48kMono[ "sample_fmt" ] = "s16";
-	wave16b48kMono[ "sample_rate" ] = "48000";
-	wave16b48kMono[ "channels" ] = "1";
+	wave16b48kMono[ Profile::avProfileCodec ] = "pcm_s16le";
+	wave16b48kMono[ Profile::avProfileSampleFormat ] = "s16";
+	wave16b48kMono[ Profile::avProfileSampleRate ] = "48000";
+	wave16b48kMono[ Profile::avProfileChannel ] = "1";
 
 	profiles.push_back( wave24b48kMono );
 	profiles.push_back( wave16b48kMono );
