6c64e058c59c0455cad2d218f31e90ec16a85cbd
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2014-07-18 11:48:06 +0200

    update to remove deprecated methods

--- src/AvTranscoder/DatasStructures/AudioDesc.cpp
+++ src/AvTranscoder/DatasStructures/AudioDesc.cpp
@@ -188,7 +188,7 @@ void AudioDesc::set( const std::string& key, const std::string& value )
 std::string AudioDesc::getAudioCodec() const
 {
 	assert( m_codecContext != NULL );
-	return m_codecContext->codec_name;
+	return avcodec_descriptor_get( m_codecContext->codec_id )->name;
 }
 
 AVCodecID AudioDesc::getAudioCodecId() const
@@ -196,7 +196,6 @@ AVCodecID AudioDesc::getAudioCodecId() const
 	assert( m_codecContext != NULL );
 	return m_codecContext->codec_id;
 }
-
 
 const size_t AudioDesc::getSampleRate() const
 {
