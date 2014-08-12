4bd847ce0d4d7a85089df44add08608a34e0b6c4
Clement Champetier <cnt@mikrosimage.eu>
2014-08-12 10:51:49 +0200

    MediaMetadatasStructures: reorder data in getDataMap()
    Update order of data for:
       * VideoProperties.
       * AudioProperties.
       * Properties.

--- src/AvTranscoder/Metadatas/MediaMetadatasStructures.cpp
+++ src/AvTranscoder/Metadatas/MediaMetadatasStructures.cpp
@@ -36,14 +36,26 @@ MetadatasMap VideoProperties::getDataMap() const
 {
 	MetadatasMap dataMap;
 
+	detail::add( dataMap, "stream id", streamId );
+	detail::add( dataMap, "codec id", codecId );
 	detail::add( dataMap, "codec name", codecName );
 	detail::add( dataMap, "codec long name", codecLongName );
+	detail::add( dataMap, "profile", profile );
 	detail::add( dataMap, "profile name", profileName );
+	detail::add( dataMap, "level", level );
 	detail::add( dataMap, "start timecode", startTimecode );
+	detail::add( dataMap, "width", width );
+	detail::add( dataMap, "height", height );
+	detail::add( dataMap, "pixel aspect ratio", sar.num / sar.den );
+	detail::add( dataMap, "display aspect ratio", dar.num / dar.den );
+	detail::add( dataMap, "dtgActiveFormat", dtgActiveFormat );
+	detail::add( dataMap, "components count", componentsCount );
 	detail::add( dataMap, "pixel type", pixelName );
 	detail::add( dataMap, "bit wise acked", bitWisePacked );
 	detail::add( dataMap, "rgb pixel", rgbPixelData );
 	detail::add( dataMap, "as alpha", asAlpha );
+	detail::add( dataMap, "chroma width", chromaWidth );
+	detail::add( dataMap, "chroma height", chromaHeight );
 	detail::add( dataMap, "endianess", endianess );
 	detail::add( dataMap, "color transfert", colorTransfert );
 	detail::add( dataMap, "colorspace", colorspace );
@@ -55,7 +67,13 @@ MetadatasMap VideoProperties::getDataMap() const
 	detail::add( dataMap, "interlaced ", isInterlaced );
 	detail::add( dataMap, "top field first", topFieldFirst );
 	detail::add( dataMap, "field order", fieldOrder);
-	detail::add( dataMap, "has B frames", hasBFrames );
+	detail::add( dataMap, "timeBase", timeBase.num / timeBase.den );
+	detail::add( dataMap, "fps", fps );
+	detail::add( dataMap, "ticksPerFrame", ticksPerFrame );
+	detail::add( dataMap, "bit rate", bitRate );
+	detail::add( dataMap, "max bit rate", maxBitRate );
+	detail::add( dataMap, "min bit rate", minBitRate );
+	detail::add( dataMap, "gop size", gopSize );
 
 	std::string gop;
 	for( size_t frameIndex = 0; frameIndex < gopStructure.size(); ++frameIndex )
@@ -65,25 +83,7 @@ MetadatasMap VideoProperties::getDataMap() const
 	}
 	detail::add( dataMap, "gop", gop );
 
-	detail::add( dataMap, "codec id", codecId );
-	detail::add( dataMap, "stream id", streamId );
-	detail::add( dataMap, "profile", profile );
-	detail::add( dataMap, "level", level );
-	detail::add( dataMap, "width", width );
-	detail::add( dataMap, "height", height );
-	detail::add( dataMap, "dtgActiveFormat", dtgActiveFormat );
-	detail::add( dataMap, "timeBase", timeBase.num / timeBase.den );
-	detail::add( dataMap, "fps", fps );
-	detail::add( dataMap, "ticksPerFrame", ticksPerFrame );
-	detail::add( dataMap, "pixel aspect ratio", sar.num / sar.den );
-	detail::add( dataMap, "display aspect ratio", dar.num / dar.den );
-	detail::add( dataMap, "bit rate", bitRate );
-	detail::add( dataMap, "max bit rate", maxBitRate );
-	detail::add( dataMap, "min bit rate", minBitRate );
-	detail::add( dataMap, "components count", componentsCount );
-	detail::add( dataMap, "chroma width", chromaWidth );
-	detail::add( dataMap, "chroma height", chromaHeight );
-	detail::add( dataMap, "gop size", gopSize );
+	detail::add( dataMap, "has B frames", hasBFrames );
 	detail::add( dataMap, "references frames", referencesFrames );
 
 	return dataMap;
@@ -93,18 +93,18 @@ MetadatasMap AudioProperties::getDataMap() const
 {
 	MetadatasMap dataMap;
 
+	detail::add( dataMap, "stream id", streamId );
+	detail::add( dataMap, "codec id", codecId );
 	detail::add( dataMap, "codec name", codecName );
 	detail::add( dataMap, "codec long name", codecLongName );
 	detail::add( dataMap, "sample format", sampleFormat );
+	detail::add( dataMap, "sample rate", sampleRate );
+	detail::add( dataMap, "bit rate", bit_rate );
+	detail::add( dataMap, "channels", channels );
 	detail::add( dataMap, "channel layout", channelLayout );
 	detail::add( dataMap, "channel name", channelName );
 	detail::add( dataMap, "channel description", channelDescription );
 
-	detail::add( dataMap, "codec id", codecId );
-	detail::add( dataMap, "stream id", streamId );
-	detail::add( dataMap, "sample rate", sampleRate );
-	detail::add( dataMap, "channels", channels );
-	detail::add( dataMap, "bit rate", bit_rate );
 
 	return dataMap;
 }
@@ -153,12 +153,6 @@ MetadatasMap Properties::getDataMap() const
 	detail::add( dataMap, "format name", formatName );
 	detail::add( dataMap, "format long name", formatLongName );
 
-	for( size_t metadataIndex = 0; metadataIndex < metadatas.size(); ++metadataIndex )
-	{
-		std::string key( "Metadatas " + metadatas.at( metadataIndex ).first );
-		detail::add( dataMap, key, metadatas.at( metadataIndex ).second );
-	}
-
 	detail::add( dataMap, "start time", startTime );
 	detail::add( dataMap, "duration", duration );
 	detail::add( dataMap, "bitrate", bitRate );
@@ -170,6 +164,11 @@ MetadatasMap Properties::getDataMap() const
 	detail::add( dataMap, "number of subtitle streams", subtitleStreams.size() );
 	detail::add( dataMap, "number of attachement streams", attachementStreams.size() );
 	detail::add( dataMap, "number of unknown streams", unknownStreams.size() );
+
+	for( size_t metadataIndex = 0; metadataIndex < metadatas.size(); ++metadataIndex )
+	{
+		detail::add( dataMap, metadatas.at( metadataIndex ).first, metadatas.at( metadataIndex ).second );
+	}
 
 	return dataMap;
 }
