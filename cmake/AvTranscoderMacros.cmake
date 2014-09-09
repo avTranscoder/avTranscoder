cdbc8020eb3e582395eed3782ca82ccd74550614
Clement Champetier <cnt@mikrosimage.eu>
2014-09-09 10:16:19 +0200

    CMake: add cmake directory to have custom utilities
    * Custom utilities are:
         * AvTranscoderMacros.cmake: macros, included in all cmake files.
         * FindFFmpeg.cmake: to get FFmpeg package (by specify a list of
    components, check the FFmpeg version, get FFmpeg from command line if
    there are parameters, etc...).

---
+++ cmake/AvTranscoderMacros.cmake
@@ -0,0 +1,4 @@
+# Get useful variables
+set(AVTRANSCODER_APP_PATH "${PROJECT_SOURCE_DIR}/app")
+set(AVTRANSCODER_SRC_PATH "${PROJECT_SOURCE_DIR}/src")
+file(GLOB_RECURSE AVTRANSCODER_SRC_FILES "AvTranscoder/*.cpp" "AvTranscoder/*.hpp")
---
+++ cmake/FindFFmpeg.cmake
@@ -0,0 +1,112 @@
+# - Find FFmpeg library
+# Find the native FFmpeg includes and library.
+# 
+# This module defines:
+# FFMPEG_FOUND
+# FFMPEG_INCLUDE_DIR - The FFmpeg include directory.
+# FFMPEG_LIBRARIES - The FFmpeg libraries.
+# 
+# For each component:
+# ${COMPONENT}_FOUND
+# ${COMPONENT}_INCLUDE_DIR
+# ${COMPONENT}_LIBRARIES
+# ${COMPONENT}_DEFINITIONS
+# ${COMPONENT}_VERSION
+#
+# If any component is specified, default components are avcodec avformat avutil.
+#
+
+### Macro: check_ffmpeg_version
+#	
+# Check FFmpeg version.
+# @todo: clean how to get current FFmpeg version.
+#
+macro(check_ffmpeg_version)
+	exec_program("ffmpeg -version 2>&1 | grep 'ffmpeg version' | sed -e 's/ffmpeg\ version\ //'"
+		OUTPUT_VARIABLE FFmpeg_CURRENT_VERSION)
+	if(${FFmpeg_CURRENT_VERSION} LESS ${FFmpeg_FIND_VERSION})
+		message(SEND_ERROR "Your FFmpeg version is too old (${FFmpeg_CURRENT_VERSION} < ${FFmpeg_FIND_VERSION}).")
+	endif()
+endmacro()
+
+
+### Macro: find_component
+#	
+# Check the given component by invoking pkgconfig and 
+# then looking up the libraries and include directories.
+# @todo: WIN and MAC OSX.
+#
+macro(find_component COMPONENT PKGCONFIG PKGOPTION LIBRARY HEADER)
+	if(NOT WIN32)
+		# use pkg-config to get the directories and then use these values
+		# in the FIND_PATH() and FIND_LIBRARY() calls
+		find_package(PkgConfig)
+		if(PKG_CONFIG_FOUND)
+			pkg_check_modules(PC_${COMPONENT} ${PKGCONFIG} ${PKGOPTION})
+		endif()
+	endif()
+
+	find_path(${COMPONENT}_INCLUDE_DIR
+		${HEADER}
+		PATH ${PC_LIB${COMPONENT}_INCLUDEDIR} ${PC_LIB${COMPONENT}_INCLUDE_DIR}
+		PATH_SUFFIXES ffmpeg libav
+	)
+
+	find_library(${COMPONENT}_LIBRARIES 
+		NAMES ${LIBRARY}
+		PATH ${PC_LIB${COMPONENT}_LIBDIR} ${PC_LIB${COMPONENT}_LIBRARY_DIRS}
+	)
+
+	set(${COMPONENT}_DEFINITIONS ${PC_${COMPONENT}_CFLAGS_OTHER} CACHE STRING "The ${COMPONENT} CFLAGS.")	
+	set(${COMPONENT}_VERSION ${PC_${COMPONENT}_VERSION} CACHE STRING "The ${COMPONENT} version number.")
+
+	# Marks the given component as found if both *_LIBRARIES AND *_INCLUDE_DIR is present.	
+	if(${COMPONENT}_LIBRARIES AND ${COMPONENT}_INCLUDE_DIR)
+		set(${COMPONENT}_FOUND TRUE)
+	else()
+		set(${COMPONENT}_FOUND FALSE)
+	endif()
+
+	mark_as_advanced(
+		${COMPONENT}_INCLUDE_DIR
+		${COMPONENT}_LIBRARIES
+		${COMPONENT}_DEFINITIONS
+		${COMPONENT}_VERSION
+	)
+endmacro()
+
+# Get FFmpeg from custom install
+if(FFMPEG_LIBRARIES AND FFMPEG_INCLUDE_DIR)
+	set(FFMPEG_FOUND TRUE)
+# Get FFmpeg from system install
+else()
+	# Check FFmpeg version
+	if(DEFINED FFmpeg_FIND_VERSION)
+		check_ffmpeg_version()
+	endif()
+
+	if(NOT FFmpeg_FIND_COMPONENTS)
+		set(FFmpeg_FIND_COMPONENTS avcodec avformat avutil)
+	endif()
+
+	# Check components and add their stuff to the FFMPEG_* vars.
+	foreach(COMPONENT ${FFmpeg_FIND_COMPONENTS})
+		# Get component name is lower cases.
+		string(TOLOWER ${COMPONENT} LOWERCOMPONENT)
+		find_component(${COMPONENT} lib${LOWERCOMPONENT} REQUIRED ${LOWERCOMPONENT} lib${LOWERCOMPONENT}/${LOWERCOMPONENT}.h)
+		# If the component is found.
+		if(${COMPONENT}_FOUND)
+			message(STATUS "Required component ${COMPONENT} present.")
+			set(FFMPEG_LIBRARIES ${FFMPEG_LIBRARIES} ${${COMPONENT}_LIBRARIES})
+			set(FFMPEG_DEFINITIONS ${FFMPEG_DEFINITIONS} ${${COMPONENT}_DEFINITIONS})
+			list(APPEND FFMPEG_INCLUDE_DIR ${${COMPONENT}_INCLUDE_DIR})
+		else()
+			message(SEND_ERROR "Required component ${COMPONENT} missing.")
+		endif()
+	endforeach()
+
+	# Build the include path with duplicates removed.
+	if(FFMPEG_INCLUDE_DIR)
+		list(REMOVE_DUPLICATES FFMPEG_INCLUDE_DIR)
+	endif()
+endif()
