e5a8da3c5ea880826fed2815d28edc31542e048f
Clement Champetier <cnt@mikrosimage.eu>
2014-10-13 14:06:46 +0200

    Option: fix setters throw runtime_error
    * These functions threw a runtime_error with error named "SUCCESS".
    * No visible consequences because we caught them when setProfile...

--- src/AvTranscoder/option/Option.cpp
+++ src/AvTranscoder/option/Option.cpp
@@ -95,7 +95,7 @@ void Option::setFlag( const std::string& flag, const bool enable )
 {
 	int64_t optVal;
 	int error = av_opt_get_int( _avContext, getName().c_str(), AV_OPT_SEARCH_CHILDREN, &optVal );
-	if( ! error )
+	if( error )
 	{
 		char err[AV_ERROR_MAX_STRING_SIZE];
 		av_strerror( error, err, AV_ERROR_MAX_STRING_SIZE );
@@ -108,7 +108,7 @@ void Option::setFlag( const std::string& flag, const bool enable )
 		optVal = optVal &~ _avOption->default_val.i64;
 
 	error = av_opt_set_int( _avContext, getName().c_str(), optVal, AV_OPT_SEARCH_CHILDREN );
-	if( error != 0 )
+	if( error )
 	{
 		char err[AV_ERROR_MAX_STRING_SIZE];
 		av_strerror( error, err, AV_ERROR_MAX_STRING_SIZE );
@@ -119,7 +119,7 @@ void Option::setFlag( const std::string& flag, const bool enable )
 void Option::setBool( const bool value )
 {
 	int error = av_opt_set_int( _avContext, getName().c_str(), value, AV_OPT_SEARCH_CHILDREN );
-	if( ! error )
+	if( error )
 	{
 		char err[AV_ERROR_MAX_STRING_SIZE];
 		av_strerror( error, err, AV_ERROR_MAX_STRING_SIZE );
@@ -130,7 +130,7 @@ void Option::setBool( const bool value )
 void Option::setInt( const int value )
 {
 	int error = av_opt_set_int( _avContext, getName().c_str(), value, AV_OPT_SEARCH_CHILDREN );
-	if( ! error )
+	if( error )
 	{
 		std::ostringstream os;
 		os << value;
@@ -146,7 +146,7 @@ void Option::setRatio( const int num, const int den )
 	ratio.num = num;
 	ratio.den = den;
 	int error = av_opt_set_q( _avContext, getName().c_str(), ratio, AV_OPT_SEARCH_CHILDREN );
-	if( ! error )
+	if( error )
 	{
 		std::ostringstream os;
 		os << num << "/" << den;
@@ -159,7 +159,7 @@ void Option::setRatio( const int num, const int den )
 void Option::setDouble( const double value )
 {
 	int error = av_opt_set_double( _avContext, getName().c_str(), value, AV_OPT_SEARCH_CHILDREN );
-	if( ! error )
+	if( error )
 	{
 		std::ostringstream os;
 		os << value;
@@ -172,7 +172,7 @@ void Option::setDouble( const double value )
 void Option::setString( const std::string& value )
 {
 	int error = av_opt_set( _avContext, getName().c_str(), value.c_str(), AV_OPT_SEARCH_CHILDREN );
-	if( ! error )
+	if( error )
 	{
 		char err[AV_ERROR_MAX_STRING_SIZE];
 		av_strerror( error, err, AV_ERROR_MAX_STRING_SIZE );
