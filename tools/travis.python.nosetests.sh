ef160952375b0dbc61638f172ba9adc6b0e85457
Clement Champetier <cnt@mikrosimage.eu>
2014-10-17 17:30:36 +0200

    Travis tests script: get assets from avTranscoder-data repository
    The assets will be necessary for some functional tests of the library.

--- tools/travis.python.nosetests.sh
+++ tools/travis.python.nosetests.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Get avtranscoder library
 export PYTHONPATH=`pwd`/build/dist/lib:`pwd`/build/dist/lib/python2.7/site-packages/:$PYTHONPATH
 
-export AVTRANSCODER_TEST_VIDEO_FILE=`pwd`/data/video/video.mxf
-export AVTRANSCODER_TEST_AUDIO_FILE=`pwd`/data/audio/audio.wav
+# Get assets
+git clone https://github.com/avTranscoder/avTranscoder-data.git
+export AVTRANSCODER_TEST_VIDEO_FILE=`pwd`/avTranscoder-data/video/BigBuckBunny/BigBuckBunny_480p_stereo.avi
+export AVTRANSCODER_TEST_AUDIO_FILE=`pwd`/avTranscoder-data/audio/frequenciesPerChannel.wav
 
+# Launch tests
 cd test/pyTest
-
 nosetests
