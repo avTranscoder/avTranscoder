d9f2e08d73f97d4cf0655a096be8e23e5415b191
Clement Champetier <cnt@mikrosimage.eu>
2014-10-20 17:35:23 +0200

    Travis: fix launch nosetests after build (linux)

--- tools/travis.linux.install.deps.sh
+++ tools/travis.linux.install.deps.sh
@@ -6,4 +6,13 @@ sudo apt-add-repository "deb http://archive.ubuntu.com/ubuntu trusty-updates mai
 sudo apt-add-repository "deb http://security.ubuntu.com/ubuntu trusty-security main restricted universe multiverse"
 sudo apt-get update -qq
 sudo apt-get install -qq gcc g++ cmake swig swig2.0
-sudo apt-get install -qq libavdevice-dev libavformat-dev libavcodec-dev libavutil-dev libswscale-dev libavresample-dev python-dev freeglut3-dev libxmu-dev doxygen
+sudo apt-get install -qq python-dev doxygen
+sudo apt-get install -qq freeglut3-dev libxmu-dev
+sudo apt-get install -qq python-nose
+
+# Build FFmpeg 2.2.9
+sudo wget https://www.ffmpeg.org/releases/ffmpeg-2.2.9.tar.bz2
+sudo bunzip2 ffmpeg-2.2.9.tar.bz2
+sudo tar -xvf ffmpeg-2.2.9.tar
+cd ffmpeg-2.2.9
+./configure --disable-yasm --enable-shared --disable-static && make && sudo make install
