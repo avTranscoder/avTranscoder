83613eb825df34c5c548d477ebfc5d3337985fec
Clement Champetier <cnt@mikrosimage.eu>
2014-10-20 09:29:43 +0200

    Travis: use the daily updated package for libav

--- tools/travis.linux.install.deps.sh
+++ tools/travis.linux.install.deps.sh
@@ -4,6 +4,7 @@ lsb_release -a
 sudo apt-add-repository "deb http://archive.ubuntu.com/ubuntu trusty main restricted universe multiverse"
 sudo apt-add-repository "deb http://archive.ubuntu.com/ubuntu trusty-updates main restricted universe multiverse"
 sudo apt-add-repository "deb http://security.ubuntu.com/ubuntu trusty-security main restricted universe multiverse"
+sudo add-apt-repository -y ppa:motumedia/libav-daily
 sudo apt-get update -qq
 sudo apt-get install -qq gcc g++ cmake swig swig2.0
 sudo apt-get install -qq python-nose
