feb56a232fd4748a8d8c9f2b6cf4e2f6290da480
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2014-10-29 13:13:47 +0100

    export directory to locate FFMpeg

--- tools/travis.python.nosetests.sh
+++ tools/travis.python.nosetests.sh
@@ -4,6 +4,9 @@
 export PYTHONPATH=`pwd`/build/dist/lib/python2.7/site-packages/:$PYTHONPATH
 export LD_LIBRARY_PATH=`pwd`/build/dist/lib:$LD_LIBRARY_PATH
 
+# FFmpeg libraries
+export LD_LIBRARY_PATH=`pwd`/usr/local/lib:$LD_LIBRARY_PATH
+
 # Get assets
 git clone https://github.com/avTranscoder/avTranscoder-data.git
 export MEDIA_MANAGER_TEST_VIDEO_FILE=`pwd`/avTranscoder-data/video/BigBuckBunny/BigBuckBunny_480p_stereo.avi
