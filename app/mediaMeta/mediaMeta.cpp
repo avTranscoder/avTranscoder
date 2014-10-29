3d92212aab5d817b71b6880ecef396d965c8067a
marco <arnaud.marcantoine@gmail.com>
2014-10-30 00:10:55 +0100

    improve mediameta with option and multi-files analysis

--- app/mediaMeta/mediaMeta.cpp
+++ app/mediaMeta/mediaMeta.cpp
@@ -1,4 +1,5 @@
 
+#include <mediaCore/Library.hpp>
 #include <mediaCore/progress/NoDisplayProgress.hpp>
 
 #include <mediaIO/file/InputFile.hpp>
@@ -8,19 +9,76 @@
 
 int main( int argc, char** argv )
 {
-	if( argc != 2 )
+	using namespace mediaengine;
+
+	bool help = false;
+	bool version = false;
+
+	for( size_t argIndex = 1; argIndex < argc; ++argIndex )
 	{
-		std::cout << "mediameta require a media filename" << std::endl;
-		return( -1 );
+		std::string arg( argv[argIndex]);
+		if( !strcmp( argv[argIndex], "-h") ||
+			!strcmp( argv[argIndex], "-H") ||
+			!strcmp( argv[argIndex], "--help") )
+			help = true;
+
+		if( !strcmp( argv[argIndex], "-V") ||
+			!strcmp( argv[argIndex], "--version" ) )
+			version = true;
 	}
 
-	using namespace mediaengine;
+	if( help )
+	{
+		std::cout << "mediameta - " << MEDIA_ENGINE_VERSION_MAJOR << "." << MEDIA_ENGINE_VERSION_MINOR << "." << MEDIA_ENGINE_VERSION_MICRO << std::endl;
+		std::cout << std::endl;
+		std::cout << "display media metadatas for each input file" << std::endl;
+		std::cout << std::endl;
+		std::cout << "mediameta [OPTIONS] [FILENAMES]" << std::endl;
+		std::cout << std::endl;
+		std::cout << "OPTIONS:" << std::endl;
+		std::cout << "-h, -H, --help: Display this help" << std::endl;
+		std::cout << "-v, --version:  Print library version and FFMpeg/Libav version." << std::endl;
+		std::cout << "                Also display license." << std::endl;
+		return 0;
+	}
+
+	if( version )
+	{
+		mediacore::Libraries libs( mediacore::getLibraries() );
+
+		for( mediacore::Libraries::iterator library = libs.begin(); library != libs.end(); ++library )
+		{
+			std::cout << std::left;
+			std::cout << std::setw( 15 ) << (*library).getName();
+			std::cout << std::setw( 10 ) << (*library).getStringVersion();
+			std::cout << std::setw( 30 ) << (*library).getLicense();
+			std::cout << std::endl;
+		}
+		return 0;
+	}
 
 	mediacore::NoDisplayProgress p;
 
-	mediaio::InputFile input( argv[1] );
-	input.analyse( p, mediaio::InputFile::eAnalyseLevelFull );
+	for( size_t argIndex = 1; argIndex < argc; ++argIndex )
+	{
+		try
+		{
+			mediaio::InputFile input( argv[argIndex] );
+			input.analyse( p, mediaio::InputFile::eAnalyseLevelFull );
 
-	// a simply metadata display
-	std::cout << input;
+			// a simply metadata display
+			std::cout << input;
+		}
+		catch( std::runtime_error& e )
+		{
+			std::cerr << "ERROR: " << e.what() << std::endl;
+		}
+		catch( ... )
+		{
+			std::cerr << "ERROR: unknown error" << std::endl;
+		}
+	}
+
+
+	return 0;
 }
