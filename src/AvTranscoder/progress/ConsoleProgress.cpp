fe509dddb27cb77c09e34c54054bf70c27aa3d43
Clement Champetier <cnt@mikrosimage.eu>
2014-12-10 11:52:09 +0100

    ConsoleProgress: fix progressDuration in case of image
    Avoid division by 0.

--- src/AvTranscoder/progress/ConsoleProgress.cpp
+++ src/AvTranscoder/progress/ConsoleProgress.cpp
@@ -12,14 +12,14 @@ ConsoleProgress::~ConsoleProgress()
 
 EJobStatus ConsoleProgress::progress( const double processedDuration, const double programDuration )
 {
+	// The program could be an image (programDuration = 0)
+	double progressDuration = programDuration ? processedDuration / programDuration : 0;
+
 	std::string progress( 80, '-' );
-	std::string done( 80.0 * processedDuration / programDuration, '#' );
+	std::string done( 80.0 * progressDuration, '#' );
 	progress.replace( 0, done.size(), done );
 
 	std::cout  << std::setprecision(2) << std::fixed << "\r[" << progress << "] " << processedDuration << "/" << programDuration << std::flush;
-
-	// if( processedFrames >= 100 )
-	// 	return avtranscoder::eJobStatusCancel;
 
 	return eJobStatusContinue;
 }
