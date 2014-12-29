b0e529f652f3fcb6133c7240e8d677a8c60a9660
Clement Champetier <cnt@mikrosimage.eu>
2014-12-29 18:24:15 +0100

    Travis: fix pyTest after build

--- tools/travis.python.nosetests.sh
+++ tools/travis.python.nosetests.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Get avtranscoder library
-export PYTHONPATH=`pwd`/build/dist/lib:`pwd`/build/dist/lib/python2.7/site-packages/:$PYTHONPATH
+export PYTHONPATH=`pwd`/build/dist/lib:`pwd`/build/dist/lib/python2.7.6/site-packages/:$PYTHONPATH
+
+# Get avtranscoder profiles
+export AVPROFILES=`pwd`/build/dist/share/ressource
 
 # Get assets
 git clone https://github.com/avTranscoder/avTranscoder-data.git
