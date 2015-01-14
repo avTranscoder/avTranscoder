645b16c6b5812d0b5541bdbee7958da5ce7cd298
Clement Champetier <cnt@mikrosimage.eu>
2015-01-14 19:03:34 +0100

    avplay app: fix preloadCodecsAndFormats

--- app/avPlay/main.cpp
+++ app/avPlay/main.cpp
@@ -1,9 +1,13 @@
+#include <AvTranscoder/common.hpp>
+
 #include "AvReader.hpp"
 #include "Window.hpp"
 
 
 int main( int argc, char** argv )
 {
+	avtranscoder::preloadCodecsAndFormats();
+
 	AvReader avReader( argv[1] );
 	Window window( avReader );
 	window.launch();
