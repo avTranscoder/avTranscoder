25b506e63bab09225d2d2b4b4d5b57378b19104e
Clement Champetier <cnt@mikrosimage.eu>
2015-01-16 13:50:29 +0100

    Travis: update video file used in pyTest

--- tools/travis.python.nosetests.sh
+++ tools/travis.python.nosetests.sh
@@ -8,7 +8,7 @@ export AVPROFILES=`pwd`/build/dist/share/ressource
 
 # Get assets
 git clone https://github.com/avTranscoder/avTranscoder-data.git
-export AVTRANSCODER_TEST_VIDEO_FILE=`pwd`/avTranscoder-data/video/BigBuckBunny/BigBuckBunny_HD.mp4
+export AVTRANSCODER_TEST_VIDEO_FILE=`pwd`/avTranscoder-data/video/BigBuckBunny/BigBuckBunny_480p_stereo.avi
 export AVTRANSCODER_TEST_AUDIO_FILE=`pwd`/avTranscoder-data/audio/frequenciesPerChannel.wav
 
 # Launch tests
