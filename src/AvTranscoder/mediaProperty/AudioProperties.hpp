d430fd279fac616b835cec2c1c481d74f1fbc902
Clement Champetier <cnt@mikrosimage.eu>
2015-01-19 19:27:20 +0100

    AudioProperties: add documentation

--- src/AvTranscoder/mediaProperty/AudioProperties.hpp
+++ src/AvTranscoder/mediaProperty/AudioProperties.hpp
@@ -34,7 +34,7 @@ public:
 
 	size_t getTicksPerFrame() const;
 	Rational getTimeBase() const;
-	double getFps() const;
+	double getFps() const;  ///< Corresponds to the number of audio samples for one video frame
 	double getDuration() const;
 
 	PropertiesMap& getMetadatas() { return _metadatas; }
