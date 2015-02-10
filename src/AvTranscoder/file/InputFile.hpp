da5438ea52bbc073ddf0a6bb8a4727e8f3c771ba
Clement Champetier <cnt@mikrosimage.eu>
2015-02-10 14:42:55 +0100

    InputFile: refactoring seekAtFrame

--- src/AvTranscoder/file/InputFile.hpp
+++ src/AvTranscoder/file/InputFile.hpp
@@ -97,6 +97,20 @@ public:
 	 **/
 	static FileProperties analyseFile( const std::string& filename, IProgress& progress, const EAnalyseLevel level = eAnalyseLevelFirstGop );
 
+private:
+	/**
+	 * @brief Get Fps from first video stream
+	 * @note if there is no video stream, return 1.
+	 */
+	double getFps();
+
+	/**
+	 * @brief Seek at a specific position (in AV_TIME_BASE units)
+	 * @note before seek, add offset of start time
+	 * @note after seek, clear buffering of streams
+	 */
+	void seek( uint64_t position );
+
 protected:
 	FormatContext _formatContext;
 	FileProperties _properties;
