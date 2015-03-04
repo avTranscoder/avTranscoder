b231cce25447449b951ff7744e4a319eae979260
Clement Champetier <cnt@mikrosimage.eu>
2015-03-04 11:39:51 +0100

    FileProperties: not throw exception for functions which return string

--- src/AvTranscoder/mediaProperty/FileProperties.cpp
+++ src/AvTranscoder/mediaProperty/FileProperties.cpp
@@ -21,21 +21,21 @@ FileProperties::FileProperties( const FormatContext& formatContext )
 std::string FileProperties::getFilename() const
 {
 	if( ! _formatContext || ! _formatContext->filename )
-		throw std::runtime_error( "unknown format context" );
+		return "unknown file name";
 	return _formatContext->filename;
 }
 
 std::string FileProperties::getFormatName() const
 {
 	if( ! _formatContext || ! _formatContext->iformat || ! _formatContext->iformat->name )
-		throw std::runtime_error( "unknown format context" );
+		return "unknown format name";
 	return _formatContext->iformat->name;
 }
 
 std::string FileProperties::getFormatLongName() const
 {
 	if( ! _formatContext || ! _formatContext->iformat || ! _formatContext->iformat->long_name )
-		throw std::runtime_error( "unknown format context" );
+		return "unknown format long name";
 	return _formatContext->iformat->long_name;
 }
 
