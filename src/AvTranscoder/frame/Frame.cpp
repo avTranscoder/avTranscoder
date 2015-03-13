2bb3bf9e09fa5059988e7816465af1372da5e6ab
Clement Champetier <cnt@mikrosimage.eu>
2015-03-13 14:44:45 +0100

    Frame: skip call of 'av_copy_packet' if depend on libav

--- src/AvTranscoder/frame/Frame.cpp
+++ src/AvTranscoder/frame/Frame.cpp
@@ -78,15 +78,18 @@ void Frame::initAVPacket()
 
 void Frame::copyAVPacket( const AVPacket& avPacket )
 {
-#if LIBAVCODEC_VERSION_INT > AV_VERSION_INT(55, 56, 108)
-	av_copy_packet( &_packet, &avPacket );
-#elif LIBAVCODEC_VERSION_INT > AV_VERSION_INT(54, 56, 0)
-	av_copy_packet( &_packet, const_cast<AVPacket*>( &avPacket ) );
-#else
+#if AVTRANSCODER_FFMPEG_DEPENDENCY
+	#if LIBAVCODEC_VERSION_INT > AV_VERSION_INT(55, 56, 108)
+		av_copy_packet( &_packet, &avPacket );
+		return;
+	#elif LIBAVCODEC_VERSION_INT > AV_VERSION_INT(54, 56, 0)
+		av_copy_packet( &_packet, const_cast<AVPacket*>( &avPacket ) );
+		return;
+	#endif
+#endif
 	// we just care about data, not side properties of AVPacket
 	initAVPacket();
 	copyData( avPacket.data, avPacket.size );
-#endif
 }
 
 }
