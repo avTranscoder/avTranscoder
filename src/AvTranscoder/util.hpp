c47b8ed6f676d1a42cdcf7885f5bdd9d7ae043a9
Clement Champetier <cnt@mikrosimage.eu>
2015-03-25 10:56:24 +0100

    util: AvExport all functions

--- src/AvTranscoder/util.hpp
+++ src/AvTranscoder/util.hpp
@@ -33,55 +33,55 @@ bool AvExport matchFormat( const std::string& format, const std::string& filenam
 *  @brief Get pixel format supported by a video codec.
 *  @param videoCodecName: the video codec name (empty if not indicated, and so get all pixel formats supported by all video codecs).
 */
-std::vector<std::string> getPixelFormats( const std::string& videoCodecName = "" );
+std::vector<std::string> AvExport getPixelFormats( const std::string& videoCodecName = "" );
 
 /**
 *  @brief Get sample format supported by an audio codec.
 *  @param audioCodecName: the audio codec name (empty if not indicated, and so get all sample formats supported by all audio codecs).
 */
-std::vector<std::string> getSampleFormats( const std::string& audioCodecName = "" );
+std::vector<std::string> AvExport getSampleFormats( const std::string& audioCodecName = "" );
 
 /**
 * @brief Get the corresponding AVPixelFormat from the pixel format name
 * @param pixelFormat: the name of the pixel format
 */
-AVPixelFormat getAVPixelFormat( const std::string& pixelFormat );
+AVPixelFormat AvExport getAVPixelFormat( const std::string& pixelFormat );
 
 /**
 * @brief Get the corresponding AVSampleFormat from the sample format name
 * @param sampleFormat: the name of the sample format
 */
-AVSampleFormat getAVSampleFormat( const std::string& sampleFormat );
+AVSampleFormat AvExport getAVSampleFormat( const std::string& sampleFormat );
 
 /**
  * @brief Get array of short/long names of all format supported by FFmpeg / libav.
  */
-NamesArray getFormatsNames();
+NamesArray AvExport getFormatsNames();
 
 /**
  * @brief Get array of short/long names of all video codec supported by FFmpeg / libav.
  */
-NamesArray getVideoCodecsNames();
+NamesArray AvExport getVideoCodecsNames();
 
 /**
  * @brief Get array of short/long names of all audio codec supported by FFmpeg / libav.
  */
-NamesArray getAudioCodecsNames();
+NamesArray AvExport getAudioCodecsNames();
 
 /**
  * @brief Get the list of options for each output format
  */
-OptionArrayMap getOutputFormatOptions();
+OptionArrayMap AvExport getOutputFormatOptions();
 
 /**
  * @brief Get the list of options for each video codec
  */
-OptionArrayMap getVideoCodecOptions();
+OptionArrayMap AvExport getVideoCodecOptions();
 
 /**
  * @brief Get the list of options for each audio codec
  */
-OptionArrayMap getAudioCodecOptions();
+OptionArrayMap AvExport getAudioCodecOptions();
 
 }
 
