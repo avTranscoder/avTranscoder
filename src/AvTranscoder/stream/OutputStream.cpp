9e0c9426010bf10e53abcf07a8d121532e90321e
Clement Champetier <cnt@mikrosimage.eu>
2015-04-21 17:50:36 +0200

    OutputStream: fix getStreamDuration (use av_stream_get_end_pts)
    Fix #165

--- src/AvTranscoder/stream/OutputStream.cpp
+++ src/AvTranscoder/stream/OutputStream.cpp
@@ -17,7 +17,12 @@ OutputStream::OutputStream( OutputFile& outputFile, const size_t streamIndex )
 double OutputStream::getStreamDuration() const
 {
 	AVStream& outputStream = _outputFile->getFormatContext().getAVStream( _streamIndex );
-	return av_q2d( outputStream.time_base ) * outputStream.cur_dts;
+#if AVTRANSCODER_FFMPEG_DEPENDENCY && LIBAVFORMAT_VERSION_INT >= AV_VERSION_INT(55, 40, 100)
+	// returns the pts of the last muxed packet, converted from timebase to seconds
+	return av_q2d( outputStream.time_base ) * av_stream_get_end_pts( &outputStream );
+#else
+	return av_q2d( outputStream.time_base ) * ( outputStream.pts.val + ( outputStream.pts.num / outputStream.pts.den ) );
+#endif
 }
 
 IOutputStream::EWrappingStatus OutputStream::wrap( const CodedData& data )
