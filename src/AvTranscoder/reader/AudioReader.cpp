9b3d44a411ce442a803f14be19248515f428ec6b
Clement Champetier <cnt@mikrosimage.eu>
2015-08-25 12:06:49 +0200

    reader: add AudioReader class

---
+++ src/AvTranscoder/reader/AudioReader.cpp
@@ -0,0 +1,67 @@
+#include "AudioReader.hpp"
+
+#include <AvTranscoder/progress/NoDisplayProgress.hpp>
+#include <AvTranscoder/mediaProperty/print.hpp>
+
+namespace avtranscoder
+{
+
+AudioReader::AudioReader( const std::string& filename, const size_t audioStreamIndex )
+	: _inputFile( filename )
+	, _audioProperties( NULL )
+	, _audioDecoder( NULL )
+	, _sourceFrame( NULL )
+	, _dstFrame( NULL )
+	, _audioTransform()
+	, _audioStreamIndex( audioStreamIndex )
+{
+	avtranscoder::NoDisplayProgress p;
+	_inputFile.analyse( p );
+	_audioProperties = &static_cast<const AudioProperties&>(_inputFile.getProperties().getStreamPropertiesWithIndex(_audioStreamIndex));
+	_inputFile.activateStream( _audioStreamIndex );
+
+	_audioDecoder = new avtranscoder::AudioDecoder( _inputFile.getStream( _audioStreamIndex ) );
+	_audioDecoder->setupDecoder();
+
+	_sourceFrame = new avtranscoder::AudioFrame( _inputFile.getStream( _audioStreamIndex ).getAudioCodec().getAudioFrameDesc() );
+
+	avtranscoder::AudioFrameDesc dstAudioFrame( _sourceFrame->desc().getSampleRate(), _sourceFrame->desc().getChannels(), _sourceFrame->desc().getSampleFormat() );
+	_dstFrame = new avtranscoder::AudioFrame( dstAudioFrame );
+}
+
+AudioReader::~AudioReader()
+{
+	delete _audioDecoder;
+	delete _sourceFrame;
+	delete _dstFrame;
+}
+
+const char* AudioReader::readNextFrame()
+{
+	return readFrameAt( _currentFrame + 1 );
+}
+
+const char* AudioReader::readPrevFrame()
+{
+	return readFrameAt( _currentFrame - 1 );
+}
+
+const char* AudioReader::readFrameAt( const size_t frame )
+{
+	_currentFrame = frame;
+	// seek
+	_inputFile.seekAtFrame( frame );
+	_audioDecoder->flushDecoder();
+	// decode
+	_audioDecoder->decodeNextFrame( *_sourceFrame );
+	_audioTransform.convert( *_sourceFrame, *_dstFrame );
+	// return buffer
+	return (const char*)_dstFrame->getData();
+}
+
+void AudioReader::printInfo()
+{
+	std::cout << *_audioProperties << std::endl;
+}
+
+}
---
+++ src/AvTranscoder/reader/AudioReader.hpp
@@ -0,0 +1,42 @@
+#ifndef _AV_TRANSCODER_AUDIOREADER_HPP
+#define _AV_TRANSCODER_AUDIOREADER_HPP
+
+#include "IReader.hpp"
+
+#include <AvTranscoder/file/InputFile.hpp>
+#include <AvTranscoder/mediaProperty/AudioProperties.hpp>
+#include <AvTranscoder/decoder/AudioDecoder.hpp>
+#include <AvTranscoder/frame/AudioFrame.hpp>
+#include <AvTranscoder/transform/AudioTransform.hpp>
+
+namespace avtranscoder
+{
+
+class AvExport AudioReader : public IReader
+{
+public:
+	AudioReader( const std::string& filename, const size_t audioStreamIndex );
+	~AudioReader();
+
+	const char* readNextFrame();
+	const char* readPrevFrame();
+	const char* readFrameAt( const size_t frame );
+
+	void printInfo();
+
+private:
+	InputFile _inputFile;
+	const AudioProperties* _audioProperties;
+	AudioDecoder* _audioDecoder;
+
+	AudioFrame* _sourceFrame;
+	AudioFrame* _dstFrame;
+
+	AudioTransform _audioTransform;
+
+	size_t _audioStreamIndex;
+};
+
+}
+
+#endif
