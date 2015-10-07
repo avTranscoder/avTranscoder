5813637c53865e148e3c62c1b4f2b541df0cad3f
Clement Champetier <cnt@mikrosimage.eu>
2015-10-07 11:40:30 +0200

    pyTest: add testCheckVideoProperties

--- test/pyTest/testProperties.py
+++ test/pyTest/testProperties.py
@@ -1,9 +1,9 @@
 import os
 
 # Check if environment is setup to run the tests
-if os.environ.get('AVTRANSCODER_TEST_AUDIO_WAVE_FILE') is None:
+if os.environ.get('AVTRANSCODER_TEST_AUDIO_WAVE_FILE') is None or os.environ.get('AVTRANSCODER_TEST_VIDEO_MP4_FILE') is None:
 	from nose.plugins.skip import SkipTest
-	raise SkipTest("Need to define environment variable AVTRANSCODER_TEST_AUDIO_WAVE_FILE")
+	raise SkipTest("Need to define environment variables AVTRANSCODER_TEST_AUDIO_WAVE_FILE / AVTRANSCODER_TEST_VIDEO_MP4_FILE")
 
 from nose.tools import *
 
@@ -63,3 +63,33 @@ def testAddImpossibleMetadata():
 
 	assert_not_in( metadata_to_check, properties.getMetadatas() )
 
+
+def testCheckVideoProperties():
+	"""
+	Check properties of a video stream.
+	"""
+	# get src file
+	inputFileName = os.environ['AVTRANSCODER_TEST_VIDEO_MP4_FILE']
+	inputFile = av.InputFile( inputFileName )
+	properties = inputFile.getProperties()
+	videoStream = properties.getVideoProperties()[0]
+
+	expectedTotalBitRate = 3249739
+	expectedVideoBitRate = 3247981
+
+	expectedCodecName = 'h264'
+	expectedWidth = 1920
+	expectedHeight = 1080
+	expectedNbFrames = 241
+	expectedDuration = 10.04
+	expectedFps = 24
+
+	assert_equals( properties.getBitRate(), expectedTotalBitRate )
+	assert_equals( videoStream.getBitRate(), expectedVideoBitRate )
+
+	assert_equals( videoStream.getCodecName(), expectedCodecName )
+	assert_equals( videoStream.getWidth(), expectedWidth )
+	assert_equals( videoStream.getHeight(), expectedHeight )
+	assert_equals( videoStream.getNbFrames(), expectedNbFrames )
+	assert_equals( round(videoStream.getDuration(), 2), expectedDuration )
+	assert_equals( videoStream.getFps(), expectedFps )
