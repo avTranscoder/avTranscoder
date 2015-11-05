368dad686a943cd3646258ef6d336dbf2b12abc9
Clement Champetier <cnt@mikrosimage.eu>
2015-11-05 12:15:55 +0100

    pyTest: added tests to check OutputFile constructor

---
+++ test/pyTest/testOutputFile.py
@@ -0,0 +1,58 @@
+import os
+
+from nose.tools import *
+
+from pyAvTranscoder import avtranscoder as av
+
+av.preloadCodecsAndFormats()
+av.Logger.setLogLevel(av.AV_LOG_QUIET)
+
+
+def testCreateOutputFileWithExtension():
+	"""
+	Create an OutputFile with a filename with extension.
+	"""
+        ext = "mov"
+	outputFileName = "testCreateOutputFileWithExtension." + ext
+	ouputFile = av.OutputFile( outputFileName )
+
+	assert_equals( ouputFile.getFilename(), outputFileName )
+	assert_equals( ouputFile.getFormatName(), ext )
+
+
+@raises(IOError)
+def testCreateOutputFileWithoutExtension():
+	"""
+	Create an OutputFile with a filename without extension.
+	"""
+	outputFileName = "testCreateOutputFileWithoutExtension"
+	ouputFile = av.OutputFile( outputFileName )
+
+	assert_equals( ouputFile.getFilename(), outputFileName )
+
+
+def testCreateOutputFileWithoutExtensionWithFormat():
+	"""
+	Create an OutputFile with a filename without extension.
+        Indicate the format.
+	"""
+        format = "mov"
+	outputFileName = "testCreateOutputFileWithoutExtensionWithFormat"
+	ouputFile = av.OutputFile( outputFileName, format )
+
+	assert_equals( ouputFile.getFilename(), outputFileName )
+	assert_equals( ouputFile.getFormatName(), format )
+
+
+def testCreateOutputFileWithoutExtensionWithMimeType():
+	"""
+	Create an OutputFile with a filename without extension.
+        Indicate the Mime Type.
+	"""
+        mimeType = "application/mp4"
+	outputFileName = "testCreateOutputFileWithoutExtensionWithMimeType"
+	ouputFile = av.OutputFile( outputFileName, "", mimeType )
+
+	assert_equals( ouputFile.getFilename(), outputFileName )
+	assert_equals( ouputFile.getFormatMimeType(), mimeType )
+
