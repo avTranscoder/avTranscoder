332febf627585458b1eb01e5c207cccd97b084bc
Clement Champetier <cnt@mikrosimage.eu>
2015-11-19 09:59:48 +0100

    CI: add AVTRANSCODER_TEST_VIDEO_MOV_FILE env variable for pyTests

--- tools/appveyor.python.nosetests.bat
+++ tools/appveyor.python.nosetests.bat
@@ -14,6 +14,7 @@ set AVPROFILES=%PWD%\build\share\avprofiles
 git clone https://github.com/avTranscoder/avTranscoder-data.git
 set AVTRANSCODER_TEST_VIDEO_AVI_FILE=%PWD%\avTranscoder-data/video\BigBuckBunny\BigBuckBunny_480p_stereo.avi
 set AVTRANSCODER_TEST_VIDEO_MP4_FILE=%PWD%\avTranscoder-data\video\BigBuckBunny\BigBuckBunny_HD.mp4
+set AVTRANSCODER_TEST_VIDEO_MOV_FILE=%PWD%\avTranscoder-data\video\BigBuckBunny\BigBuckBunny_640p_h264.mov
 set AVTRANSCODER_TEST_AUDIO_WAVE_FILE=%PWD%\avTranscoder-data\audio\frequenciesPerChannel.wav
 set AVTRANSCODER_TEST_AUDIO_MOV_FILE=%PWD%\avTranscoder-data\video\BigBuckBunny\BigBuckBunny_1080p_5_1.mov
 
