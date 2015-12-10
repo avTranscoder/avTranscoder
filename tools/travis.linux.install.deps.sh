d1c2876f80935ae7b2b7507673dad090aed2b2db
cchampet <cl.champetier@gmail.com>
2015-12-10 10:42:48 +0100

    Travis: added -k option when build dependencies
    ‘--keep-going’: continue as much as possible after an error.

--- tools/travis.linux.install.deps.sh
+++ tools/travis.linux.install.deps.sh
@@ -11,14 +11,14 @@ if [ ! -d "${DEPENDENCY_INSTALL_PATH}/lib/" ]; then
         bunzip2 ffmpeg-${DEPENDENCY_VERSION}.tar.bz2
         tar -xvf ffmpeg-${DEPENDENCY_VERSION}.tar
         cd ffmpeg-${DEPENDENCY_VERSION}
-        ./configure --prefix=${DEPENDENCY_INSTALL_PATH} --disable-yasm --enable-shared --disable-static && make && make install
+        ./configure --prefix=${DEPENDENCY_INSTALL_PATH} --disable-yasm --enable-shared --disable-static && make -k && make install
 
     elif [[ ${DEPENDENCY_NAME} == "libav" ]]; then
 
         wget https://libav.org/releases/libav-${DEPENDENCY_VERSION}.tar.gz
         tar -xvf libav-${DEPENDENCY_VERSION}.tar.gz
         cd libav-${DEPENDENCY_VERSION}
-        ./configure --prefix=${DEPENDENCY_INSTALL_PATH} --disable-yasm --enable-shared --disable-static && make && make install
+        ./configure --prefix=${DEPENDENCY_INSTALL_PATH} --disable-yasm --enable-shared --disable-static && make -k && make install
 
     fi
 
