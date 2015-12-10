21459688bd3b2bf7c1bca0ad3d40d31b82aabee6
cchampet <cl.champetier@gmail.com>
2015-12-10 10:25:05 +0100

    Travis: build libav on MacOSX since the brew formula does not install the libav libraries

--- tools/travis.osx.install.deps.sh
+++ tools/travis.osx.install.deps.sh
@@ -19,5 +19,10 @@ brew install freeglut
 if [[ ${DEPENDENCY_MODE} == "ffmpeg" ]]; then
     brew install ffmpeg
 elif [[ ${DEPENDENCY_MODE} == "libav" ]]; then
-    brew install libav
+    export LIBAV_VERSION=11.3
+    wget https://libav.org/releases/libav-${LIBAV_VERSION}.tar.gz
+    tar -xvf libav-${LIBAV_VERSION}.tar.gz
+    cd libav-${LIBAV_VERSION}
+    ./configure --disable-yasm --enable-shared --disable-static && make -k && make install
+
 fi
