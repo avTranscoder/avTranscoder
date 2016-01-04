ae2e1d99189112b0a58beaf0f62586e2103478a8
Clement Champetier <cnt@mikrosimage.eu>
2016-01-04 18:07:24 +0100

    Transcoder: updated doc of the processing policy

--- src/AvTranscoder/transcoder/Transcoder.hpp
+++ src/AvTranscoder/transcoder/Transcoder.hpp
@@ -17,14 +17,14 @@ namespace avtranscoder
 {
 
 /**
- * @brief Enum to set a policy of how we manage the transcode in case of several streams.
- * eProcessMethodShortest: stop transcode at the end of the shortest stream.
- * eProcessMethodLongest: stop transcode at the end of the longest stream.
- * eProcessMethodBasedOnStream: stop transcode at the end of an indicated stream (@see _indexBasedStream attribute of
+ * @brief Enum to set a policy of how we manage the process in case of several streams.
+ * eProcessMethodShortest: stop the process at the end of the shortest stream.
+ * eProcessMethodLongest: stop the process at the end of the longest stream.
+ * eProcessMethodBasedOnStream: stop the process at the end of an indicated stream (@see _indexBasedStream attribute of
  * Transcoder).
- * eProcessMethodBasedOnDuration: stop transcode at the end of an indicated duration, in seconds (@see _outputDuration
+ * eProcessMethodBasedOnDuration: stop the process at the end of an indicated duration, in seconds (@see _outputDuration
  * attribute of Transcoder).
- * eProcessMethodInfinity: stop transcode by outside of avTranscoder (streaming mode)
+ * eProcessMethodInfinity: stop the process by outside of avTranscoder (streaming mode)
  */
 enum EProcessMethod
 {
@@ -229,9 +229,9 @@ private:
     std::vector<StreamTranscoder*> _streamTranscoders;          ///< All streams of the output media file after process.
     std::vector<StreamTranscoder*> _streamTranscodersAllocated; ///< Streams allocated inside the Transcoder (has ownership)
 
-    ProfileLoader _profileLoader; ///< Objet to get existing profiles, and add new ones for the Transcoder.
+    ProfileLoader _profileLoader;   ///< Objet to get existing profiles, and add new ones for the Transcoder.
 
-    EProcessMethod _eProcessMethod; ///< Transcoding policy
+    EProcessMethod _eProcessMethod; ///< Processing policy
     size_t
         _mainStreamIndex;  ///< Index of stream used to stop the process of transcode in case of eProcessMethodBasedOnStream.
     float _outputDuration; ///< Duration of output media used to stop the process of transcode in case of
