d441d2562fb266cb5925a243e9b952d666c8eb9f
Clement Champetier <cnt@mikrosimage.eu>
2016-01-21 16:43:49 +0100

    filter: added Filter class
    Describe a filter and its options.

---
+++ src/AvTranscoder/filter/Filter.hpp
@@ -0,0 +1,40 @@
+#ifndef _AV_TRANSCODER_FILTER_FILTER_HPP_
+#define _AV_TRANSCODER_FILTER_FILTER_HPP_
+
+#include <AvTranscoder/common.hpp>
+
+struct AVFilter;
+struct AVFilterContext;
+
+namespace avtranscoder
+{
+
+/**
+ * @brief Describe a filter and its options.
+ **/
+class AvExport Filter
+{
+public:
+    Filter(const std::string& name, const std::string& options = "", const std::string& instanceName = "");
+    ~Filter();
+
+    std::string getName() const;
+    std::string getOptions() const { return _options; }
+    std::string getInstanceName() const { return _instanceName; }
+
+#ifndef SWIG
+    AVFilter& getAVFilter() { return *_filter; }
+    AVFilterContext* getAVFilterContext() { return _context; }
+
+    void setAVFilterContext(AVFilterContext* newContext) { _context = newContext; }
+#endif
+
+private:
+    AVFilter* _filter;
+    AVFilterContext* _context;
+    std::string _options;
+    std::string _instanceName;
+};
+}
+
+#endif
