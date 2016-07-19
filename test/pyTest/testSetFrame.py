1d84193f357504a8590027f5eadeaa74e08a5077
Clement Champetier <cnt@mikrosimage.eu>
2016-07-19 16:59:22 +0200

    pyTest: fixed testSetFrame

--- test/pyTest/testSetFrame.py
+++ test/pyTest/testSetFrame.py
@@ -23,7 +23,7 @@ def testSetVideoFrame():
 
     # create transcoder and add a video stream
     transcoder = av.Transcoder( ouputFile )
-    transcoder.addStream( encodingProfile )
+    transcoder.addGeneratedStream( encodingProfile )
     videoDecoder = transcoder.getStreamTranscoder( 0 ).getCurrentDecoder()
 
     # start process
@@ -69,7 +69,7 @@ def testSetAudioFrame():
 
     # create transcoder and add a video stream
     transcoder = av.Transcoder( ouputFile )
-    transcoder.addStream(  "wave24b48kmono" )
+    transcoder.addGeneratedStream( "wave24b48kmono" )
     audioDecoder = transcoder.getStreamTranscoder( 0 ).getCurrentDecoder()
 
     # start process
