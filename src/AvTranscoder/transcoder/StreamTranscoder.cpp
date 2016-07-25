9c6ee8aa83b29f76f1e056834c9892b796cd4df8
Valentin Noel <van@mikrosimage.eu>
2016-07-25 17:45:20 +0200

    StreamTranscoder: unref the filtergraph output AVFrame after it has been converted

--- src/AvTranscoder/transcoder/StreamTranscoder.cpp
+++ src/AvTranscoder/transcoder/StreamTranscoder.cpp
@@ -550,6 +550,10 @@ bool StreamTranscoder::processTranscode()
         LOG_DEBUG("Convert")
         _transform->convert(*_filteredData, *_transformedData);
 
+        // free the filtered AVFrame now it has been converted into another one
+        // @see the av_buffersink_get_frame documentation
+        av_frame_unref(&_filteredData->getAVFrame());
+
         LOG_DEBUG("Encode")
         _outputEncoder->encodeFrame(*_transformedData, data);
     }
