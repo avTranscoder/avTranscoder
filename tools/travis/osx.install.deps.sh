02a6ce7b834dfa39b98166f2b8635150246f67dd
Valentin NOEL <valentin.noel@media-io.com>
2020-01-22 14:34:41 +0100

    Travis: force python version

--- tools/travis/osx.install.deps.sh
+++ tools/travis/osx.install.deps.sh
@@ -9,11 +9,15 @@ brew update
 brew install gcc cmake swig doxygen
 
 # To launch nosetests
-brew install python
-pip install nose
+brew install pyenv
+pyenv install 3.5.0
+pyenv local 3.5.0
+pip3 install nose
 
 # To build avplay application
 brew install freeglut
 
 # Main dependency
+
+ls -l ${DEPENDENCY_INSTALL_PATH}/lib/
 ./tools/travis/linux.install.deps.sh
