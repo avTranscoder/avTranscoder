11b534782c70133a6e4bb9f6be3d89d6e42d501d
Valentin NOEL <valentin.noel@media-io.com>
2020-01-22 11:47:04 +0100

    Add Travis nose tests verbose

--- tools/travis/python.nosetests.sh
+++ tools/travis/python.nosetests.sh
@@ -20,4 +20,9 @@ export AVTRANSCODER_TEST_IMAGE_PNG_FILE=`pwd`/avTranscoder-data/image/BigBuckBun
 export AVTRANSCODER_TEST_IMAGE_JPG_FILE=`pwd`/avTranscoder-data/image/BigBuckBunny/title_anouncement.thumbnail.jpg
 
 # Launch tests
-nosetests3 -w ${TRAVIS_BUILD_DIR}/test/pyTest --with-coverage --nocapture > progress.txt
+nosetests3 -w ${TRAVIS_BUILD_DIR}/test/pyTest --with-coverage --nocapture --verbose > progress.txt
+
+TEST_EXIT_CODE=$?
+if [ $TEST_EXIT_CODE != 0 ]; then cat progress.txt; fi
+
+exit($TEST_EXIT_CODE)
